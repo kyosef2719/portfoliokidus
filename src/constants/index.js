0e2c1bab832d143932021f5b744d49d98c47ee18
Kidus Yosef <kidyo2719@gmail.com>
2024-04-12 15:07:10 +0300

    Edited & revised portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am autodidactic & passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2015 - Hithertho",
+    role: "Manging Director",
+    company: "Kidus Yosef Foreign Trade Agent.",
+    description: `Managing business development & project management activities,Working on financial & various commodities deals in collaboration with trusted agents all over the world, Sourcing reliable suppliers for different goods & commodities,Consulting importers & exporters.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2003 - 2015",
+    role: "Managing Director",
+    company: "Afro-Tibeb Business Group.",
+    description: `Managed business development activities,Set & attained organization's goals & objectives, Directed & secured many awards from different organzations for the supply of vaious goods, Ensured delivery of goods as the scope,schedule, cost & quality.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2001 - 2003",
+    role: "Import Control Head",
+    company: "The Motor & Engineering company(MOENCO)",
+    description: `Managed the overall import activities of the company,Directed & monitored import control department activities,Introduced working procedures for the department, Ensured the proper cost calculation of each & every imported goods & vehicles, Maintained & enhanced smooth working relationship with banks, customs, clearing & forwarding agents.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "1993 - 2001",
+    role: "Marketing & foreign purchase officer",
+    company: "SIEMENS PVT LTD Co.,",
+    description: `Performed the company import activities,Improved the company smooth working relationship with banks, customs, shipping lines & insurance companies, Ensured & excuted market assesment and cost calculation activities, Foresighted & implemented importing goods via Djibouti port & saved the company from losing multimillion dollars.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bole Road,Purple Building,House No. 333/27 ",
+  phoneNo: "+251-911-244923 ",
+  email: "kidyo2719@gmail.com",
+};
